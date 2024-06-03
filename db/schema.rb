d25e2045443fdf41ee9d467e3b6bc46eae80de15
Alexander Sinelnik <ssinelnik@bk.ru>
2024-06-03 14:28:28 +0500

    Rollback all migrations and destroy model after 4th review

--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_162150) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_31_172538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: :cascade do |t|
-    t.string "body", null: false
-    t.boolean "correct", default: false, null: false
-    t.bigint "question_id_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id_id"], name: "index_answers_on_question_id_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.bigint "test_id_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["test_id_id"], name: "index_categories_on_test_id_id"
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string "body", null: false
-    t.bigint "test_id_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["test_id_id"], name: "index_questions_on_test_id_id"
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.bigint "test_id_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["test_id_id"], name: "index_users_on_test_id_id"
-  end
-
-  add_foreign_key "answers", "questions", column: "question_id_id"
-  add_foreign_key "categories", "tests", column: "test_id_id"
-  add_foreign_key "questions", "tests", column: "test_id_id"
-  add_foreign_key "users", "tests", column: "test_id_id"
 end
